c8634c1d1e4c0d3570ff0fdc6f27cf2432998e93
Your Name <you@example.com>
2023-01-31 10:02:30 +0400

    init

--- Frontend/boho-app/src/components/Home/HomeAbout.tsx
+++ Frontend/boho-app/src/components/Home/HomeAbout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Img from '../../assets/images/Home/BreakfastB12.jpg'
 function HomeAbout() {
     return (
@@ -8,13 +9,17 @@ function HomeAbout() {
                     <div className="col-6">
                         <img className='home-about-image' src={Img} alt="" />
                     </div>
-                    <div className="col-6">
+                    <div className="col-6 home-about-texts">
                         <div className="home-about-content">
-                            <h1>Boho tea & wine room</h1>
-                            <h1>Exclusive food for you</h1>
-                            <p>About restaurant</p>
-                            <p>Opaleye yellowtail snapper, velvet catfish, graveldiver banded killifish, Old World rivuline catalufa eagle ray Moorish idol. Herring smelt barbeled dragonfish, tommy ruff.</p>
-                            <p>Queen danio velvet catfish Sacramento blackfish, bullhead shark, Colorado squawfish Russian sturgeon clown triggerfish swamp-eel paradise fish. Hake cookie-cutter shark silver carp hawkfish snipe eel armorhead catfish, moray eel silverside! Bluegill toadfish, orangespine unicorn fish. Manta Ray Moorish idol</p>
+                            <h1 className='home-about-title'>BOHO TEA & WINE ROOM</h1>
+                            <h1 className='home-about-title'>EXCLUSIVE FOOD FOR YOU</h1>
+                            <p className='home-about-label'>About restaurant</p>
+                            <p className='home-about-text-up'>Opaleye yellowtail snapper, velvet catfish, graveldiver banded killifish, Old World rivuline catalufa eagle ray Moorish idol. Herring smelt barbeled dragonfish, tommy ruff.</p>
+                            <p className='home-about-text-down'>Queen danio velvet catfish Sacramento blackfish, bullhead shark, Colorado squawfish Russian sturgeon clown triggerfish swamp-eel paradise fish. Hake cookie-cutter shark silver carp hawkfish snipe eel armorhead catfish, moray eel silverside! Bluegill toadfish, orangespine unicorn fish. Manta Ray Moorish idol</p>
+                            <div className='home-about-button-area'>
+                                <Link to={'/about'} className="home-about-button">Explore more</Link>
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
